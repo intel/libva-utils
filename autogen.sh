c9af683d6cb1f7d79ac0a300cc0f586569466738
Daniel Charles <daniel.charles@intel.com>
2016-09-21 11:31:46 -0700

    libva-utils: initial commit
    libva-utils is a new git repository that will host sample applications
    for libva, conformance tests and others relevant to libva
    
    It is using the same version as libva and it should follow libva version.
    It is intended to be released together with libva
    It has the same license as libva
    
    Initial functionality ports the vainfo application from libva repository.
    The git history for it will remain on libva.  To port vainfo, also the
    a common libva-display library was ported. The original tests for encoder
    and decoder are also ported to this repository. debian folder for vainfo
    was also ported
    
    The basic tests are going to be substituted by a new set of tests based
    on Gtest framework.
    
    libva-utils will dynamically link libva, use pkg-config to compile and
    link properly.
    
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>

---
+++ autogen.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Copyright (c) 2007 Intel Corporation. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sub license, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice (including the
+# next paragraph) shall be included in all copies or substantial portions
+# of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+# IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+PROJECT="libva-utils"
+
+test -n "$srcdir" || srcdir="`dirname \"$0\"`"
+test -n "$srcdir" || srcdir=.
+
+if ! test -f "$srcdir/configure.ac"; then
+    echo "Failed to find the top-level $PROJECT directory"
+    exit 1
+fi
+
+olddir="`pwd`"
+cd "$srcdir"
+
+mkdir -p m4
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+    echo "*** No autoreconf found ***"
+    exit 1
+else
+    autoreconf -v --install || exit $?
+fi
+
+cd "$olddir"
+
+if test -z "$NOCONFIGURE"; then
+    $srcdir/configure "$@" && echo "Now type 'make' to compile $PROJECT."
+fi
