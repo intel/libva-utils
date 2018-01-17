db37306549ecb452471fd694a08dbdded440fa32
U. Artie Eoff <ullysses.a.eoff@intel.com>
2018-01-17 14:14:44 -0800

    test/createsurfaces: refactor and fix test bugs
    Take into account bitfield attributes when creating
    config and surfaces.
    
    Use new fixture methods for greater control in test
    cases.
    
    Fixes #91
    Fixes #92
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/test_va_api_createsurfaces.cpp
+++ test/test_va_api_createsurfaces.cpp
@@ -24,44 +24,150 @@
 
 #include "test_va_api_fixture.h"
 
+#include <functional>
+#include <sstream>
+
 namespace VAAPI {
 
 // The following tests will operate on supported profile/entrypoint
-// combinations that the driver does support, there is no real need
-// to report SKIPPED tests as those cases are considered on the
-// get_create_config use cases and properly handled.
+// combinations that the driver does support.
+
+class VAAPISurfaceFixture
+    : public VAAPIFixture
+{
+public:
+    VAAPISurfaceFixture(const VAProfile& p, const VAEntrypoint& e)
+        : VAAPIFixture()
+        , profile(p)
+        , entrypoint(e)
+    { }
+
+protected:
+    const VAProfile& profile;
+    const VAEntrypoint& entrypoint;
+
+    virtual void SetUp()
+    {
+        VAAPIFixture::SetUp();
+
+        doInitialize();
+        doGetMaxValues();
+    }
+
+    virtual void TearDown()
+    {
+        VAAPIFixture::TearDown();
+
+        doTerminate();
+    }
+
+    void testWithSupportedConfigAttributes(
+        const std::function<void (const ConfigAttributes&)>& test)
+    {
+        ConfigAttributes supported;
+        getConfigAttributes(profile, entrypoint, supported);
+
+        // create config with each individual supported attribute
+        for (const auto& attrib : supported) {
+            const auto match = g_vaConfigAttribBitMasks.find(attrib.type);
+            if (match != g_vaConfigAttribBitMasks.end()) {
+                // it's a bitfield attribute
+                const BitMasks& masks = match->second;
+                for (const auto mask : masks) { // for all bitmasks
+                    if ((attrib.value & mask) == mask) { // supported value
+                        const ConfigAttributes attribs(
+                            1, {type : attrib.type, value : mask });
+                        createConfig(profile, entrypoint, attribs);
+                        test(attribs);
+                        destroyConfig();
+                    }
+                }
+            } else {
+                // it's a standard attribute
+                const ConfigAttributes attribs(1, attrib);
+                createConfig(profile, entrypoint, attribs);
+                test(attribs);
+                destroyConfig();
+            }
+        }
+    }
+
+    void testWithSupportedSurfaceAttributes(
+        const std::function<void (const SurfaceAttributes&)>& test)
+    {
+        SurfaceAttributes supported;
+        querySurfaceAttributes(supported);
+
+        const uint32_t drmMemMask = VA_SURFACE_ATTRIB_MEM_TYPE_KERNEL_DRM
+            | VA_SURFACE_ATTRIB_MEM_TYPE_DRM_PRIME
+            | VA_SURFACE_ATTRIB_MEM_TYPE_DRM_PRIME_2;
+
+        // create surfaces for each supported attribute
+        for (const auto& attrib : supported) {
+            const auto match = g_vaSurfaceAttribBitMasks.find(attrib.type);
+            if (match != g_vaSurfaceAttribBitMasks.end()) {
+                // it's a bitfield attribute
+                ASSERT_EQ(attrib.value.type, VAGenericValueTypeInteger);
+                uint32_t bitfield(0);
+                const BitMasks& masks = match->second;
+                for (const auto mask : masks) { // for all bitmasks
+                    if ((attrib.value.value.i & mask) == mask) {
+                        // supported value
+                        bitfield |= mask;
+
+                        if ((attrib.type == VASurfaceAttribMemoryType)
+                            and (drmMemMask & mask) == mask) {
+                            // skip drm memory types for now as it requires much
+                            // more setup
+                            continue;
+                        } else {
+                            VASurfaceAttrib maskAttrib = attrib;
+                            maskAttrib.value.value.i = mask;
+                            const SurfaceAttributes attribs = {maskAttrib,};
+                            test(attribs);
+                        }
+                    }
+                }
+                // ensure we processed all supported values
+                EXPECT_EQ(bitfield, (uint32_t)attrib.value.value.i);
+            } else {
+                // it's a standard attribute
+                const SurfaceAttributes attribs = {attrib,};
+                test(attribs);
+            }
+        }
+    }
+};
+
+typedef ::testing::WithParamInterface<
+    std::tuple<VAProfile, VAEntrypoint> > QuerySurfacesParamInterface;
 
 class VAAPIQuerySurfaces
-    : public VAAPIFixture
-    , public ::testing::WithParamInterface<std::tuple<VAProfile, VAEntrypoint> >
+    : public QuerySurfacesParamInterface
+    , public VAAPISurfaceFixture
 {
 public:
     VAAPIQuerySurfaces()
-    {
-        m_vaDisplay = doInitialize();
-    }
-
-    virtual ~VAAPIQuerySurfaces()
-    {
-        doTerminate();
-    }
+        : QuerySurfacesParamInterface()
+        , VAAPISurfaceFixture(
+            ::testing::get<0>(GetParam()),
+            ::testing::get<1>(GetParam()))
+    { }
 };
 
 TEST_P(VAAPIQuerySurfaces, QuerySurfacesWithConfigAttribs)
 {
-    const VAProfile& profile        = ::testing::get<0>(GetParam());
-    const VAEntrypoint& entrypoint  = ::testing::get<1>(GetParam());
+    const auto test = [&](const ConfigAttributes& ca) {
+        SurfaceAttributes attribs;
+        querySurfaceAttributes(attribs);
+    };
 
-    doGetMaxValues();
     doQueryConfigProfiles();
-
     if (doFindProfileInList(profile)) {
         doQueryConfigEntrypoints(profile);
         if (doFindEntrypointInList(entrypoint)) {
             // profile and entrypoint are supported
-            doFillConfigAttribList();
-            doGetConfigAttributes(profile, entrypoint);
-            doQuerySurfacesWithConfigAttribs(profile, entrypoint);
+            testWithSupportedConfigAttributes(test);
         } else {
             // entrypoint not supported
             doLogSkipTest(profile, entrypoint);
@@ -74,17 +180,14 @@ TEST_P(VAAPIQuerySurfaces, QuerySurfacesWithConfigAttribs)
 
 TEST_P(VAAPIQuerySurfaces, QuerySurfacesNoConfigAttribs)
 {
-    const VAProfile& profile        = ::testing::get<0>(GetParam());
-    const VAEntrypoint& entrypoint  = ::testing::get<1>(GetParam());
-
-    doGetMaxValues();
     doQueryConfigProfiles();
-
     if (doFindProfileInList(profile)) {
         doQueryConfigEntrypoints(profile);
         if (doFindEntrypointInList(entrypoint)) {
             // profile and entrypoint are supported
-            doQuerySurfacesNoConfigAttribs(profile, entrypoint);
+            createConfig(profile, entrypoint);
+            SurfaceAttributes attribs;
+            querySurfaceAttributes(attribs);
         } else {
             // entrypoint not supported
             doLogSkipTest(profile, entrypoint);
@@ -100,40 +203,57 @@ INSTANTIATE_TEST_CASE_P(
     ::testing::Combine(::testing::ValuesIn(g_vaProfiles),
         ::testing::ValuesIn(g_vaEntrypoints)));
 
+typedef typename ::testing::WithParamInterface<std::tuple<
+    VAProfile, VAEntrypoint, Resolution>> CreateSurfacesParamInterface;
+
 class VAAPICreateSurfaces
-    : public VAAPIFixture
-    , public ::testing::WithParamInterface<
-          std::tuple<VAProfile, VAEntrypoint, Resolution> >
+    : public CreateSurfacesParamInterface
+    , public VAAPISurfaceFixture
 {
 public:
     VAAPICreateSurfaces()
-    {
-        m_vaDisplay = doInitialize();
-    }
+        : CreateSurfacesParamInterface()
+        , VAAPISurfaceFixture(
+            ::testing::get<0>(GetParam()),
+            ::testing::get<1>(GetParam()))
+        , resolution(::testing::get<2>(GetParam()))
+    { }
 
-    virtual ~VAAPICreateSurfaces()
-    {
-        doTerminate();
-    }
+protected:
+    const Resolution& resolution;
 };
 
 TEST_P(VAAPICreateSurfaces, CreateSurfacesWithConfigAttribs)
 {
-    const VAProfile& profile        = ::testing::get<0>(GetParam());
-    const VAEntrypoint& entrypoint  = ::testing::get<1>(GetParam());
-    const Resolution& resolution    = ::testing::get<2>(GetParam());
+    // VA_RT_FORMAT_YUV420 is considered the universal supported format by
+    // drivers
+    unsigned format = VA_RT_FORMAT_YUV420;
 
-    doGetMaxValues();
+    const auto testSurfaces = [&](const SurfaceAttributes& attribs) {
+        Surfaces surfaces(10, VA_INVALID_SURFACE);
+        createSurfaces(surfaces, format, resolution, attribs);
+        destroySurfaces(surfaces);
+    };
+
+    const auto test = [&](const ConfigAttributes& attribs) {
+        const auto match = std::find_if(attribs.begin(), attribs.end(),
+            [](const VAConfigAttrib& a)
+                { return a.type == VAConfigAttribRTFormat; });
+        if (match != attribs.end()) {
+            format = match->value;
+        }
+        testWithSupportedSurfaceAttributes(testSurfaces);
+
+        // reset format to default
+        format = VA_RT_FORMAT_YUV420;
+    };
+
     doQueryConfigProfiles();
-
     if (doFindProfileInList(profile)) {
         doQueryConfigEntrypoints(profile);
         if (doFindEntrypointInList(entrypoint)) {
             // profile and entrypoint are supported
-            doFillConfigAttribList();
-            doGetConfigAttributes(profile, entrypoint);
-            doQuerySurfacesWithConfigAttribs(profile, entrypoint);
-            doCreateSurfaces(profile, entrypoint, resolution);
+            testWithSupportedConfigAttributes(test);
         } else {
             // entrypoint not supported
             doLogSkipTest(profile, entrypoint);
@@ -146,19 +266,20 @@ TEST_P(VAAPICreateSurfaces, CreateSurfacesWithConfigAttribs)
 
 TEST_P(VAAPICreateSurfaces, CreateSurfacesNoConfigAttrib)
 {
-    const VAProfile& profile        = ::testing::get<0>(GetParam());
-    const VAEntrypoint& entrypoint  = ::testing::get<1>(GetParam());
-    const Resolution& resolution    = ::testing::get<2>(GetParam());
+    const auto test = [&](const SurfaceAttributes& attribs) {
+        Surfaces surfaces(10, VA_INVALID_SURFACE);
+        createSurfaces(surfaces, VA_RT_FORMAT_YUV420, resolution, attribs);
+        destroySurfaces(surfaces);
+    };
 
-    doGetMaxValues();
     doQueryConfigProfiles();
-
     if (doFindProfileInList(profile)) {
         doQueryConfigEntrypoints(profile);
         if (doFindEntrypointInList(entrypoint)) {
             // profile and entrypoint are supported
-            doQuerySurfacesNoConfigAttribs(profile, entrypoint);
-            doCreateSurfaces(profile, entrypoint, resolution);
+            createConfig(profile, entrypoint);
+            testWithSupportedSurfaceAttributes(test);
+            destroyConfig();
         } else {
             // entrypoint not supported
             doLogSkipTest(profile, entrypoint);
@@ -171,18 +292,24 @@ TEST_P(VAAPICreateSurfaces, CreateSurfacesNoConfigAttrib)
 
 TEST_P(VAAPICreateSurfaces, CreateSurfacesNoAttrib)
 {
-    const VAProfile& profile        = ::testing::get<0>(GetParam());
-    const VAEntrypoint& entrypoint  = ::testing::get<1>(GetParam());
-    const Resolution& resolution    = ::testing::get<2>(GetParam());
-
-    doGetMaxValues();
     doQueryConfigProfiles();
-
     if (doFindProfileInList(profile)) {
         doQueryConfigEntrypoints(profile);
         if (doFindEntrypointInList(entrypoint)) {
             // profile and entrypoint are supported
-            doCreateSurfaces(profile, entrypoint, resolution);
+            createConfig(profile, entrypoint);
+
+            Surfaces surfaces(10, VA_INVALID_SURFACE);
+            Resolution minRes, maxRes;
+            getMinMaxSurfaceResolution(minRes, maxRes);
+            if (resolution.isWithin(minRes, maxRes)) {
+                createSurfaces(surfaces, VA_RT_FORMAT_YUV420, resolution);
+                destroySurfaces(surfaces);
+            } else {
+                // TODO: test createsurfaces with invalid resolution
+            }
+
+            destroyConfig();
         } else {
             // entrypoint not supported
             doLogSkipTest(profile, entrypoint);
