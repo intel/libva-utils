02a4198ebe33ea370a277b52066c694c68acc8c5
tiantian <tiantian.wang@intel.com>
2018-04-02 14:37:33 +0800

    add vainfo support in android
    Signed-off-by: tiantian <tiantian.wang@intel.com>

--- decode/Android.mk
+++ decode/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	mpeg2vldemo.cpp		\
 	../common/va_display.c	\
-
+	../common/va_display_android.cpp
 
 LOCAL_CFLAGS += \
     -DANDROID
@@ -30,7 +30,7 @@ LOCAL_SRC_FILES := \
 	loadjpeg.c \
 	tinyjpeg.c \
 	../common/va_display.c	\
-
+	../common/va_display_android.cpp
 
 LOCAL_CFLAGS += \
     -DANDROID
